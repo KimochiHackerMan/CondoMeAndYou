45dbc22da48699875b473885a0c55cf5acc2b41d
KimochiHackerMan <132354070+KimochiHackerMan@users.noreply.github.com>
2024-11-13 23:13:07 +0800

    register.php

--- register.php
+++ register.php
@@ -17,7 +17,7 @@ if(isset($_POST['signUp'])){
      }
      else{
         $insertQuery="INSERT INTO users(firstName,lastName,email,password)
-                       VALUES (,'$firstName','$lastName','$email','$password')";
+                       VALUES ('$firstName','$lastName','$email','$password')";
             if($conn->query($insertQuery)==TRUE){
                 header("location: index.php");
             }
